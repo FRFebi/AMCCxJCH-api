48aaa11c90eadf89b34c8139da3b2676fb084f25
jude <jude@team-xyz.com>
2023-11-17 17:39:21 +0700

    fixing issue cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 var jsonParser = bodyParser.json()
 // var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
